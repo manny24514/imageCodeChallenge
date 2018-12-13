d730aa2f701b8412908e450aa88a14479a83292a
MANUEL HERNANDEZ <rl106926@rlas-macair-36.local>
2018-12-13 11:32:37 -0600

    Initial Commit

---
+++ imageCodeChallenge/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  imageCodeChallenge
+//
+//  Created by MANUEL HERNANDEZ on 9/10/18.
+//  Copyright © 2018 MANUEL HERNANDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var myImage = #imageLiteral(resourceName: "kisspng-chanel-t-shirt-louis-vuitton-logo-monogram-gucci-logo-5ac3e9d2d442d0.9789954115227888188694")
+   
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = myImage
+        
+        
+    }
+
+    
+    }
+
+
+
+
